3ab3930b344aa8eaef22858b05acad3374ddaa9b
sergey <serjjiniuss@gmail.com>
2026-02-14 21:49:30 +0400

    Express+Sequelize

--- util/database.js
+++ util/database.js
@@ -1,19 +1,25 @@
-"use strict";
-
 // const mysql = require("mysql2");
+
 // const pool = mysql.createPool({
 //     host: process.env.DB_HOST,
 //     user: process.env.DB_USER,
 //     database: process.env.DB_NAME,
 //     password: process.env.DB_PASSWORD,
 // });
-// module.exports = pool.promise();
-var Sequelize = require("sequelize");
 
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
 require("dotenv").config();
 
-var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: "mysql",
-  host: process.env.DB_HOST
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: "mysql",
+        host: process.env.DB_HOST,
+    },
+);
+
 module.exports = sequelize;
