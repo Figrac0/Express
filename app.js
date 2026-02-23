1ff1c62585147081223e6e5112fefebcbe308070
sergey <serjjiniuss@gmail.com>
2026-02-23 13:16:36 +0400

    Error-Handling

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ app.use(csurfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
     if (!req.session.userId) {
         return next();
     }
@@ -52,23 +58,33 @@ app.use((req, res, next) => {
     User.findById(req.session.userId)
         .then((user) => {
             if (!user) {
+                if (!user) {
+                    return next();
+                }
                 return next();
             }
             req.user = user;
             next();
         })
-        .catch((err) => console.log(err));
-});
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+        .catch((err) => {
+            // throw new Error(err);
+            next(new Error(err));
+        });
 });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
+app.get("/500", errorController.get500);
+
+app.use((error, req, res, next) => {
+    res.status(500).render("505", {
+        pageTitle: "Error",
+        path: "/505",
+        isAuthenticated: req.session.isLoggedIn,
+    });
+});
 
 app.use(errorController.get404);
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -24,7 +24,7 @@ exports.postAddProduct = (req, res, next) => {
         console.log(errors.array());
         return res.status(422).render("admin/edit-product", {
             pageTitle: "Add Product",
-            path: "/admin/edit-product",
+            path: "/admin/add-product",
             editing: false,
             hasError: true,
             product: {
@@ -53,7 +53,10 @@ exports.postAddProduct = (req, res, next) => {
             res.redirect("/admin/products");
         })
         .catch((err) => {
-            console.log(err);
+            // res.redirect("/500");
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
         });
 };
 
@@ -78,7 +81,11 @@ exports.getEditProduct = (req, res, next) => {
                 validationErrors: [],
             });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -122,7 +129,11 @@ exports.postEditProduct = (req, res, next) => {
                 res.redirect("/admin/products");
             });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 exports.getProducts = (req, res, next) => {
@@ -137,7 +148,11 @@ exports.getProducts = (req, res, next) => {
                 path: "/admin/products",
             });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -147,5 +162,9 @@ exports.postDeleteProduct = (req, res, next) => {
             console.log("DESTROYED PRODUCT");
             res.redirect("/admin/products");
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
